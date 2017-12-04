4948546007d45ebe68117251e5e1e383341af402
Grant Visser <grant.visser@entelect.co.za>
2017-12-04 09:50:53 +0200

    Simple POC

--- ExploreJHB/ExploreJHB/Startup.cs
+++ ExploreJHB/ExploreJHB/Startup.cs
@@ -27,7 +27,7 @@ namespace ExploreJHB
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync("Hello ASP.NET Core!");
             });
         }
     }
